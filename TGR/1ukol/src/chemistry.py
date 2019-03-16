0f37f6a738692149b2ecc9c928d0968430ab76cf
Tibor Dudlák <tdudlak@redhat.com>
2019-03-16 22:20:00 +0100

    Fix print_dict usage and create structure

--- TGR/1ukol/src/chemistry.py
+++ TGR/1ukol/src/chemistry.py
@@ -24,8 +24,64 @@ def get_compounds(data):
     return compound1, compound2
 
 
+def subtask0(compound1, compound2):
+    return str(len(compound1) == len(compound2)).lower()
+
+
+def subtask1(compound1, compound2):
+    return str(len(compound1) != len(compound2)).lower()
+
+
+def subtask2(compound1, compound2):
+    return str(len(compound1) != len(compound2)).lower()
+
+
+def subtask3(compound1, compound2):
+    return str(len(compound1) != len(compound2)).lower()
+
+
+def subtask4(compound1, compound2):
+    return str(len(compound1) != len(compound2)).lower()
+
+
+def subtask5(compound1, compound2):
+    return str(len(compound1) != len(compound2)).lower()
+
+
+def subtask6(compound1, compound2):
+    return str(len(compound1) != len(compound2)).lower()
+
+
+def subtask7(compound1, compound2):
+    return str(len(compound1) != len(compound2)).lower()
+
+
+def subtask8(compound1, compound2):
+    return str(len(compound1) != len(compound2)).lower()
+
+
+def subtask9(compound1, compound2):
+    return str(len(compound1) != len(compound2)).lower()
+
+
 def run(data):
     compound1, compound2 = get_compounds(data)
-
-    parse.print_dict(compound1)
-    parse.print_dict(compound2)
+    parse.print_graph(compound1)
+    parse.print_graph(compound2)
+    print("* |U1| = |U2|: " + subtask0(compound1, compound2))
+    print("* |H1| = |H2|: " + subtask2(compound1, compound2))
+    print("* Jsou-li u, v sousední uzly, pak i (u), (v) jsou sousední uzly: " +
+          subtask2(compound1, compound2))
+    print("* Grafy mají stejnou posloupnost stupňů uzlů: " +
+          subtask3(compound1, compound2))
+    print("* Pak pro každý uzel v z U platí")
+    print("\t– stupeň uzlu v je roven stupni uzlu φ(v): " +
+          subtask4(compound1, compound2))
+    print("\t– množina stupňů sousedů uzlu v je rovna množině stupňů "
+          "sousedů uzlu φ(v): " + subtask5(compound1, compound2))
+    print("* Pak pro každý sled platí")
+    print("\t– obraz sledu je opět sled: " + subtask6(compound1, compound2))
+    print("\t– obraz tahu je opět tah: " + subtask7(compound1, compound2))
+    print("\t– obraz cesty je opět cesta: " + subtask8(compound1, compound2))
+    print("\t– délka sledu zůstává zachována: " +
+          subtask9(compound1, compound2))
