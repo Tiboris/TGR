49fe584c60ec5b45e10ea918608a8ca812288add
Tibor Dudlák <tdudlak@redhat.com>
2019-03-30 16:46:26 +0100

    Add Stack and Queue

--- TGR/2ukol/src/power.py
+++ TGR/2ukol/src/power.py
@@ -1,11 +1,27 @@
 #!/usr/python3
 from graphs import Graph
 from nodes import Transformer
+from structures import Stack
+
+conn_delim = " - "
+weight_delim = ": "
+
+
+def transformers_status_ok(transformers):
+    transformers.print_graph()
+    s = Stack()
+
+    for vertex in transformers.vertices:
+        s.push(vertex)
+        s.print_content()
+
+    return True
 
 
 def run(data):
-    transformers = Graph(data, Transformer, " - ", ": ")
-    transformers.print_graph()
+    transformers = Graph(data, Transformer, conn_delim, weight_delim)
 
-    for vertex in transformers.vertices:
-        print (vertex, transformers.vertices[vertex])
+    if transformers_status_ok(transformers):
+        print("Stav site OK")
+    else:
+        print("Stav site ERROR")
