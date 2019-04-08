f0e487d0165da3124c3c914cd8073377ccf4be41
Tibor Dudlák <tdudlak@redhat.com>
2019-04-08 19:02:56 +0200

    Create output for car; TODO Djikstra

--- TGR/3ukol/src/car.py
+++ TGR/3ukol/src/car.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 from graphs import Graph
 from nodes import Transformer
+from parse import invert_dict
 
 
 def run(data):
-    components = Graph(data, Transformer, " - ", ": ")
-    components.print_graph()
+    start = "Vy"
+    car = Graph(data, Transformer, " - ", ": ")
+    endpoints = car.nodes.keys()
+
+    dic = {}
+    for endpoint in endpoints:
+        cost = len(car.find_shortest_path(start, endpoint))
+        # TODO ^^^ cost, path = djixtra(start, endpoint)
+        dic[endpoint] = 0 - cost
+
+    weights, inverted = invert_dict(dic)
+    for weight in reversed(weights):
+        for endpoint in sorted(inverted[weight]):
+            print(f"{endpoint}: {weight}")
