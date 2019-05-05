a4e67e08ccd7e1b1cfab17603ef844abefac37a8
Tibor Dudlák <tdudlak@redhat.com>
2019-05-05 19:16:23 +0200

    Add maps and test.sh

--- TGR/4ukol/src/parking.py
+++ TGR/4ukol/src/parking.py
@@ -5,7 +5,8 @@ from graphs import City
 def run(data):
     city = City(data)
     # print(city)
-    # city.print_map()
+    city.print_map()
+    print("-------\nOutput:")
     key_of_one_minimal = min(city.costs, key=city.costs.get)
     for mapping in city.combinations[key_of_one_minimal]:
         print(
